c35eae31112215df326b227f1bb840802251973d
scoyne <scoyne@bw.edu>
2018-04-15 11:26:57 -0400

    completed assignment-bloccit-crud-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-
+  resources :advertisements
   resources :posts
 
   get 'about' => 'welcome#about'
 
-  root 'welcome#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'welcome#index'
 end
