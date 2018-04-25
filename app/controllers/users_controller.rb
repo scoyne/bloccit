501b8007ef76a314891556b9deccc74c0a2b52e7
scoyne <scoyne@bw.edu>
2018-04-24 20:16:51 -0400

    completed assignment-bloccit-authentication-confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,4 +22,21 @@ class UsersController < ApplicationController
     end
   end
 
+# CONFIRM action
+  def confirm
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+  end
+
+# User params
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # This makes `user_params()` available in the view.
+    helper_method :user_params
+
 end
