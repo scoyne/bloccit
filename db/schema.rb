3ab47dc45194f648a7eaaf9e088451c4b2b950f8
scoyne <scoyne@bw.edu>
2018-04-29 09:18:26 -0400

    completed bloccit-authorization

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180427231825) do
+ActiveRecord::Schema.define(version: 20180429123208) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 20180427231825) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rold"
+    t.integer "role"
   end
 
 end
